124231a9ba2368c995bb91a74d1bd448faa7be9e
ThomasBabato <65108595+ThomasBabato@users.noreply.github.com>
2021-07-08 16:11:07 +0200

    20 07 08
    Reinizializzato tutto.
    Ho creato la sezione senza login, mancano ancora i contenuti.

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/helloworld")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+# NO LOGIN PAGES
+
+@app.route("/")
+def nologin_Home():
+    return render_template("nologin_homepage.html")
+
+@app.route("/attivita")
+def nologin_Attivita():
+    return render_template("nologin_attivita.html")
+
+@app.route("/locali")
+def nologin_Locali():
+    return render_template("nologin_locali.html")
+
+@app.route("/contatti")
+def nologin_Contatti():
+    return render_template("nologin_contatti.html")
