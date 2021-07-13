3c0c03c86ac4c78616b9168f25598ae9ca56bbba
donnyz <Dindonnyz@gmail.com>
2021-07-13 14:08:30 +0200

    commit

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from sqlalchemy import create_engine,MetaData,inspect
+from sqlalchemy import create_engine,MetaData,inspect,ForeignKeyConstraint,PrimaryKeyConstraint
 from sqlalchemy import inspect,Table, Column, Integer, String, MetaData, ForeignKey,Float,DateTime,Date,Boolean
 from  sqlalchemy_utils.functions import database
 
@@ -9,7 +9,7 @@ app = Flask(__name__)
 #generic config for mysqldb
 #
 #NOTA : stimao usando sqlalchemy core, visto che con orm l'altra volta stavamo avendo problemi e appunto era una punto
-#       extra, per ora ho ricreato quasi tutto con core, me lo devo Vedere ancora un pò per quanto riguarda le fk  
+#       extra, per ora ho ricreato quasi tutto con core, me lo devo Vedere ancora un pò per quanto riguarda le fk
 # per connettersi con mysql  bisogna usare il dialect pmysql quindi nel momento in cui si crea l'istanza dell'engine :
 # engie = create_engine("mysql+pymysql://user:passuser@localhost/dbname")
 #controlliamo che al primo avvio della web app sul pc, il db esista o meno. in caso negativo lo si crea.
@@ -18,7 +18,7 @@ app = Flask(__name__)
 
 engine = create_engine("mysql+pymysql://anonimo:Anonimo1%@localhost", echo=True)
 
-if not database.database_exists("mysql+pymysql://anonimo:Anonimo1%@localhost/gym"):
+if not database.database_exists("mysql+pymysql://anonimo:Anonimo1%@localhost//University-project-BD/gym"):
     con = engine.connect()
     con.execute("commit")
     con.execute("create database gym")
@@ -45,10 +45,17 @@ utenti = Table('utenti',metadata,
                )
 #TODO: aggiungere colonne istruttore id e istruttre
 corsi = Table('corsi',metadata,
-              Column('id_corso', Integer, primary_key=True, autoincrement=True),
+              Column('id_corso', Integer,autoincrement=True),
               Column('nome', String(25)),
+              PrimaryKeyConstraint('id_corso','nome',name='corsi_pk'),
               Column('descrizione', String(150))
               )
+
+corsi_seguiti = Table('corsi_seguiti',metadata,
+                    Column('utente',None,ForeignKey('utenti.id_utente'),nullable=False),
+                    Column('corso_id',None,ForeignKey('corsi.id_corso')),
+                    )
+
 
 #TODO :
 locali = Table('locali',metadata,
@@ -64,6 +71,8 @@ lezioni = Table('lezioni',metadata,
                 Column('giorno',Date),
                 Column('ora',DateTime)
                 )
+
+corso_
 
 metadata.create_all(engine)
 
