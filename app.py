d88ea0ecae44a6e9c7aa3a4ed7222943943c3c01
donnyz <Dindonnyz@gmail.com>
2021-07-14 03:26:46 +0200

    last commit
    si scrive sul db. da sisteare le select com form e rivere le chiavi primarie di tutte le tabelle., perchè registra più utenti con  la stessa mail.  infine vedere come funziona flash

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ from sqlalchemy import create_engine, MetaData, inspect, ForeignKeyConstraint, P
 from sqlalchemy import inspect,Table, Column, Integer, String, MetaData, ForeignKey,Float,DateTime,Date,Boolean
 from  sqlalchemy_utils.functions import database
 from sqlalchemy_utils.types import email
+from db import *
+
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY']='THIS IS SECRET KEY1121312'
 
 #generic config for mysqldb
 #
@@ -17,68 +19,6 @@ app = Flask(__name__)
 #crare i vari utenti/ruolisulla propria macchian in mysql.
 #Nota : ho creato sulla mia macchina un utente anonimo che potrà vedere le varie info della web app senza registrarsi.
 
-engine = create_engine("mysql+pymysql://anonimo:Anonimo1%@localhost", echo=True)
-
-if not database.database_exists("mysql+pymysql://anonimo:Anonimo1%@localhost//University-project-BD/gym"):
-    con = engine.connect()
-    con.execute("commit")
-    con.execute("create database gym")
-    #con.execute("set global activate_all_roles_on_login = on")  # attivazione di tutti i ruoli  (da decommentare dopo!)
-    con.close()
-else:
-    #TODO: da elimianre? momentaneamente lascio, può tornare utile.
-    print("il database esiste già")
-
-engine = create_engine("mysql+pymysql://anonimo:Anonimo1%@localhost/gym")
-metadata = MetaData()
-
-###########
-#sezione dedicata alla creaazione delle tabelle in mysql tramite sql alchemy core:
-#TODO: aggiungere la colonna corsi.
-utenti = Table('utenti',metadata,
-               Column('id_utente',Integer,autoincrement=True),
-               Column('nome',String(25)),
-               Column('cognome',String(25)),
-               Column('email',String(30)),
-               Column('telefono',String(15)),
-               Column('password',String(16),nullable=False),
-               Column('Tampone',Boolean),
-               PrimaryKeyConstraint('id_utente',name='utenti_pk')
-               #Column('ruolo',enumerate((0,'gestore'),(1,'iStruttori'),('2','utenti')))
-               )
-#TODO: aggiungere colonne istruttore id e istruttre
-corsi = Table('corsi',metadata,
-              Column('id_corso', Integer,autoincrement=True),
-              Column('nome', String(25)),
-              PrimaryKeyConstraint('id_corso','nome',name='corsi_pk'),
-              Column('descrizione', String(150))
-              )
-
-corsi_seguiti = Table('corsi_seguiti',metadata,
-                    Column('utente',None,ForeignKey('utenti.id_utente'),nullable=False),
-                    Column('corso_id',None,ForeignKey('corsi.id_corso'),nullable=False),
-                    )
-
-
-#TODO :
-locali = Table('locali',metadata,
-               Column('id_locale', Integer, primary_key=True, autoincrement=True),
-               Column('nome', String(25)),
-               Column('mq', Float),
-               Column('capienza', Integer)
-              )
-#TODO: aggiungere i collegamenti tra corso e id
-lezioni = Table('lezioni',metadata,
-                Column('id_lezione', Integer, primary_key=True, autoincrement=True),
-                Column('descrizione', String(120)),
-                Column('giorno',Date),
-                Column('ora',DateTime)
-                )
-
-
-metadata.create_all(engine)
-
-###########
 
 # NO LOGIN PAGES
 
@@ -113,23 +53,24 @@ def Register():
 @app.route("/registratiFunzione", methods=['GET', 'POST'])
 def RegisterFunction():
     con=engine.connect() #connessione aperta
-    con.execute("SET TRANSACTION ISOLATION LEVEL SERIALIZABLE") #livello di isolamento SERIALIZABLE
-    con.execute("START TRANSACTION") #inizio transazione
+    #con.execute("SET TRANSACTION ISOLATION LEVEL SERIALIZABLE") #livello di isolamento SERIALIZABLE
+    #con.execute("START TRANSACTION") #inizio transazione
     #controlliamo se esiste un utente registrato con la stessa email facendo una query al db
-    s = select(utenti.c.email).where(utenti.c.email==email)
-    result_checkEmail = con.execute (s, email=request.form['email']).fetchone()
-    flash(request.form['email'])
+   # s= utenti.query.filter_by(utenti.c.email=email=request.form['email'])
+    #result_checkEmail = con.execute ("Select * from utenti where utenti.id_utente=12")
     #verifichiamo se la query ha dato almeno un risultato
     #in caso positivo diamo un feedback all'utente e lo facciamo tornare alla pagina di registrazione
-    if result_checkEmail:
-        flash("Email già usata da un altro utente")
-        render_template("register.html")
-
-
+     #   flash("Email già usata da un altro utente")
+     #   render_template("register.html")
     #creiamo la query per inserire il nuovo utente
-    s = utenti.insert().values(nome,cognome,email,password) #TODO: Aggiungere ruolo!
-    #result_insertUtente = con.execute(s, request.form['nome'], request.form['cognome'], request.form['email'], request.form['password'])
-    result_insertUtente = con.execute(s, nome=request.form['nome'], cognome=request.form['cognome'], email=request.form['email'], password=request.form['password'])
-    con.close() #connessione chiusa
+
+    con = engine.connect()  # connessione aperta
+    s = utenti.insert().values(nome=request.form['nome'], cognome=request.form['cognome'],
+                               email=request.form['email'], telefono=request.form['telefono'],
+                               password=request.form['password'])
+    con.execute(s)
+    #con.execute("delete from utenti")
+    con.close()
+
     flash("Registrazione eseguito correttamente!")
     return "Registrato!"
---
+++ db.py
@@ -0,0 +1,60 @@
+from sqlalchemy import create_engine, MetaData, inspect, ForeignKeyConstraint, PrimaryKeyConstraint, select
+from sqlalchemy import inspect,Table, Column, Integer, String, MetaData, ForeignKey,Float,DateTime,Date,Boolean
+from  sqlalchemy_utils.functions import database
+
+
+engine = create_engine("mysql+pymysql://anonimo:Anonimo1%@localhost", echo=True)
+
+if not database.database_exists("mysql+pymysql://anonimo:Anonimo1%@localhost//University-project-BD/gym"):
+    con = engine.connect()
+    con.execute("commit")
+    con.execute("create database if not exists gym ")
+    con.execute("set global activate_all_roles_on_login = on")  # attivazione di tutti i ruoli  (da decommentare dopo!)
+    con.close()
+
+engine = create_engine("mysql+pymysql://anonimo:Anonimo1%@localhost/gym")
+metadata = MetaData()
+
+utenti = Table('utenti',metadata,
+           Column('id_utente',Integer,autoincrement=True),
+           Column('nome',String(25)),
+           Column('cognome',String(25)),
+           Column('email',String(30)),
+           Column('telefono',String(15)),
+           Column('password',String(16),nullable=False),
+           Column('Tampone',Boolean),
+           PrimaryKeyConstraint('id_utente',name='utenti_pk')
+           #Column('ruolo',enumerate((0,'gestore'),(1,'iStruttori'),('2','utenti')))
+           )
+#TODO: aggiungere colonne istruttore id e istruttre
+corsi = Table('corsi',metadata,
+          Column('id_corso', Integer,autoincrement=True),
+          Column('nome', String(25)),
+          PrimaryKeyConstraint('id_corso','nome',name='corsi_pk'),
+          Column('descrizione', String(150))
+          )
+
+corsi_seguiti = Table('corsi_seguiti',metadata,
+                Column('utente',None,ForeignKey('utenti.id_utente'),nullable=False),
+                Column('corso_id',None,ForeignKey('corsi.id_corso'),nullable=False),
+                )
+
+
+#TODO :
+locali = Table('locali',metadata,
+               Column('id_locale', Integer, primary_key=True, autoincrement=True),
+               Column('nome', String(25)),
+               Column('mq', Float),
+               Column('capienza', Integer)
+              )
+#TODO: aggiungere i collegamenti tra corso e id
+lezioni = Table('lezioni',metadata,
+                Column('id_lezione', Integer, primary_key=True, autoincrement=True),
+                Column('descrizione', String(120)),
+                Column('giorno',Date),
+                Column('ora',DateTime)
+                )
+
+
+metadata.create_all(engine)
+
