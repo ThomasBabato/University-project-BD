69b433d91e7458a48ef9dca0d33bd96f44e4f876
donnyz <Dindonnyz@gmail.com>
2021-07-21 02:52:50 +0200

    database sistemato
    sistemati ruoli, permessi e inserito  il gestore della palestra

--- db.py
+++ db.py
@@ -1,7 +1,7 @@
 import enum
 
 from sqlalchemy import create_engine, MetaData, inspect, ForeignKeyConstraint, PrimaryKeyConstraint, select
-from sqlalchemy import inspect,Table, Column, Integer, String, MetaData, ForeignKey,Float,DateTime,Date,Boolean,Enum
+from sqlalchemy import inspect,Table, Column, Integer, String, MetaData, ForeignKey,Float,DateTime,Date,Boolean,Enum,text
 from  sqlalchemy_utils.functions import database
 
 class Ruoli(enum.Enum):
@@ -13,12 +13,35 @@ class Ruoli(enum.Enum):
 
 
 
-engine = create_engine("mysql+pymysql://anonimo:Anonimo1%@localhost", echo=False)
+engine = create_engine("mysql+pymysql://anonimo:Anonimo1%@localhost", echo=True)
 
 if not database.database_exists("mysql+pymysql://anonimo:Anonimo1%@localhost//University-project-BD/gym"):
     con = engine.connect()
     con.execute("commit")
     con.execute("create database if not exists gym ")
+    query = text("CREATE ROLE IF NOT EXISTS :x,:y,:z,:c")
+    con.execute(query, {"x" : "Anonimo","y":"Gestore","z":"Istruttore","c":"Cliente"})
+
+    query=text("GRANT INSERT,DELETE,UPDATE ON gym.* to :x")
+    con.execute(query,{"x":"Gestore"})
+
+    #Istruttre
+    query=text("GRANT INSERT,DELETE,UPDATE ON gym.corsi to :x")
+    con.execute(query,{"x":"Istruttore"})
+
+    query=text("GRANT INSERT,DELETE,UPDATE ON gym.lezioni to :x")
+    con.execute(query,{"x":"Istruttore"})
+
+    query=text("GRANT INSERT,DELETE,UPDATE ON gym.utenti to :x")
+    con.execute(query,{"x":"Cliente"})
+
+    query=text("GRANT INSERT,DELETE,UPDATE ON gym.corsi to :x")
+    con.execute(query,{"x":"Cliente"})
+
+    query=text("GRANT INSERT,DELETE,UPDATE ON gym.corsi_seguiti to :x")
+    con.execute(query,{"x":"Cliente"})
+
+
     con.execute("set global activate_all_roles_on_login = on")  # attivazione di tutti i ruoli  (da decommentare dopo!)
     con.close()
 
@@ -77,4 +100,11 @@ prenotazioni= Table('prenotazioni',metadata,
 
 
 metadata.create_all(engine)
+
+con=engine.connect()
+
+query = text("Insert into utenti values (:a, :b,:c, :d, :e,:f,:g,:h)")
+
+con.execute(query, {"a": "0", "b":"Antonio", "c":"Rossi","d":"antorossi@gmail.com" , "e" : "333321312","f":"mipalestra", "g":False,"h":"1"})
+
 
